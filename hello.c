9d28869a99b633aec8e37ae050477b0a5af13c12
Swapnamoy Bhowmick <77017479+swapnamoy17@users.noreply.github.com>
2024-09-30 01:42:54 +0000

    comments

--- hello.c
+++ hello.c
@@ -5,9 +5,6 @@
 int
 main(void)
 {
-  // Use the write system call to print "Hello world\n" to stdout (file descriptor 1)
   printf(1, "Hello world\n");
-
-  // Exit the program gracefully
   exit();
 }
